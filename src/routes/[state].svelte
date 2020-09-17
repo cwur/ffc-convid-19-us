ee2ff5c531925cfe01dd8eab638fe993b8c81cd0
cwurexit <cwur@mailbox.org>
2020-09-17 15:36:43 +0200

    error page working

--- src/routes/[state].svelte
+++ src/routes/[state].svelte
@@ -1,15 +1,35 @@
 <script context="module">
-    export async function preload(page) {
-        return { state: page.params['state'] };
+  import stateNames from "../data/stateNames";
+
+  export async function preload(page) {
+    console.log(stateNames, page.params["state"]);
+    const state = page.params["state"];
+    if (
+      stateNames.find((stateName) => stateName.abbreviation === state) ===
+      undefined
+    ) {
+      this.error(404, "State Not Found");
+      return;
     }
 
+    try {
+      return { state };
+    } catch (e) {
+      this.error(
+        500,
+        "There was an error in calling the api, please try again in 5 minutes."
+      );
+      return;
+    }
+  }
 </script>
-<script>
-    import CovidStat from '../components/CovidStat.svelte';
-    import CovidChart from '../components/CovidChart.svelte';
-    import TableContainer from '../components/TableContainer.svelte';
 
-    export let state;
+<script>
+  import CovidStat from "../components/CovidStat.svelte";
+  import CovidChart from "../components/CovidChart.svelte";
+  import TableContainer from "../components/TableContainer.svelte";
+
+  export let state;
 </script>
 
 <svelte:head>
@@ -17,9 +37,9 @@
 </svelte:head>
 
 <div class="section header">
-    <div class="container">
-      <h1>Covid 19 - {state}</h1>
-    </div>
+  <div class="container">
+    <h1>Covid 19 - {state}</h1>
+  </div>
 </div>
 
 <CovidStat />
