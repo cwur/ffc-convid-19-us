24ef2bccb59ab246be497846100aa5a890f4e2d0
cwurexit <cwur@mailbox.org>
2020-09-17 13:36:52 +0200

    pages and components working

---
+++ src/components/TableContainer.svelte
@@ -0,0 +1,9 @@
+<h1>Table Container</h1>
+
+<TableFilter />
+<Table />
+
+<script>
+    import TableFilter from './TableFilter.svelte';
+    import Table from './Table.svelte';
+</script>
---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<CovidStat />
+<CovidChart />
+<TableContainer />
+
+<script>
+    import CovidStat from '../components/CovidStat.svelte';
+    import CovidChart from '../components/CovidChart.svelte';
+    import TableContainer from '../components/TableContainer.svelte';
+</script>
