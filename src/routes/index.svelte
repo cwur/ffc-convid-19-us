20b7d86a3427ec95a4c70b2a2270e92b2de912fa
cwurexit <cwur@mailbox.org>
2020-09-17 14:18:13 +0200

    page titles working

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,13 @@
+<svelte:head>
+  <title>Covid 19 US Tracker</title>
+</svelte:head>
+
+<div class="section header">
+    <div class="container">
+      <h1>Covid 19 - US</h1>
+    </div>
+</div>
+
 <CovidStat />
 <CovidChart />
 <TableContainer />
