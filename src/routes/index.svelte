3bde29ffdb9d75a898021b0abb0ce5c507f4f995
cwurexit <cwur@mailbox.org>
2020-09-18 10:05:19 +0200

    chart parsing working

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,39 +1,41 @@
+<script context="module">
+  import requests from "../data/requests.js";
+
+  export async function preload() {
+    try {
+      const usStats = await requests.usStats();
+      const historicUS = await requests.historicUS();
+      return { usStats, historicUS };
+    } catch (e) {
+      console.log(e);
+    }
+  }
+</script>
+
+<script>
+  import CovidStat from "../components/CovidStat.svelte";
+  import CovidChart from "../components/CovidChart.svelte";
+  import TableContainer from "../components/TableContainer.svelte";
+  import About from "./about.svelte";
+  import Error from "./_error.svelte";
+  import Layout from "./_layout.svelte";
+
+  export let usStats;
+  export let historicUS;
+  console.log(historicUS, "historicUS");
+  console.log(usStats, "usStats");
+</script>
+
 <svelte:head>
   <title>Covid 19 US Tracker</title>
 </svelte:head>
 
 <div class="section header">
-    <div class="container">
-      <h1>Covid 19 - US</h1>
-    </div>
+  <div class="container">
+    <h1>Covid 19 - US</h1>
+  </div>
 </div>
 
-<CovidStat {...usStats}/>
+<CovidStat {...usStats} />
 <CovidChart />
 <TableContainer />
-
-<script context="module">
-    import requests from '../data/requests.js';
-
-    export async function preload() {
-        try {
-            const usStats = await requests.usStats();
-
-            return { usStats };
-        } catch(e) {
-            console.log(e);
-        }
-    }
-
-</script>
-<script>
-    import CovidStat from '../components/CovidStat.svelte';
-    import CovidChart from '../components/CovidChart.svelte';
-    import TableContainer from '../components/TableContainer.svelte';
-import About from './about.svelte';
-import Error from './_error.svelte';
-import Layout from './_layout.svelte';
-
-    export let usStats;
-    console.log(usStats, 'usStats');
-</script>
